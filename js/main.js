cce520e3fe68baa117858de367935ecafbdde957
flkoch <flkoch@dev.fkoch.ch>
2016-06-29 12:21:05 +0200

    bootstrap and fotawesome included in custom styles.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// jQuery
+$.getScript('../dependencies/bootstrap/js/carousel.js', function()
+{
+    // script is now loaded and executed.
+    // put your dependent JS here.
+});
+// jQuery
+$.getScript('../dependencies/bootstrap/js/dropdown.js', function()
+{
+    // script is now loaded and executed.
+    // put your dependent JS here.
+});
+// jQuery
+$.getScript('../dependencies/bootstrap/js/tab.js', function()
+{
+    // script is now loaded and executed.
+    // put your dependent JS here.
+});
