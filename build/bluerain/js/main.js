c97634962aaf2373fa1e1c204771d3ae0f54e793
Florian Koch <flkoch@dev.fkoch.ch>
2017-07-14 10:05:41 +0200

    Revert change of font path

--- build/bluerain/js/main.js
+++ build/bluerain/js/main.js
@@ -4,24 +4,6 @@
  * @author Florian Koch
  * @license GPL-3.0
  */
-/**
- * @name bluerain - A simple, custom build Drupal template.
- * @version v0.1.2
- * @author Florian Koch
- * @license GPL-3.0
- */
-/**
- * @name bluerain - A simple, custom build Drupal template.
- * @version v0.1.2
- * @author Florian Koch
- * @license GPL-3.0
- */
-/**
- * @name bluerain - A simple, custom build Drupal template.
- * @version v0.1.1
- * @author Florian Koch
- * @license GPL-3.0
- */
 // jQuery
 $.getScript('../dependencies/bootstrap/js/carousel.js', function()
 {
