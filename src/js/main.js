35dbc3706150659f4ad9f082d250b92cc07817e1
Florian Koch <flkoch@dev.fkoch.ch>
2018-05-15 10:54:45 +0200

    Implemented collapsable mobile menu with language switcher.

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
-// jQuery
-$.getScript('../dependencies/bootstrap/js/carousel.js', function()
-{
-    // script is now loaded and executed.
-    // put your dependent JS here.
-});
-// jQuery
-$.getScript('../dependencies/bootstrap/js/dropdown.js', function()
-{
-    // script is now loaded and executed.
-    // put your dependent JS here.
-});
-// jQuery
-$.getScript('../dependencies/bootstrap/js/tab.js', function()
-{
-    // script is now loaded and executed.
-    // put your dependent JS here.
-});
+"use strict";
+
+var toggleButtons = document.getElementsByClassName('toggle-btn');
+
+for (var i = 0; i < toggleButtons.length; i++) {
+    var toggleButton = toggleButtons[i];
+    toggleButton.addEventListener('click', function(event) {
+        var targets = document.querySelectorAll(toggleButton.getAttribute('data-target'));
+        for (var j = 0; j < targets.length; j++) {
+            var target = targets[j];            
+            if (target.classList) {
+                target.classList.toggle("opened");
+            } else {
+                // For IE9
+                var classes = target.className.split(" ");
+                var i = classes.indexOf("opened");
+            
+                if (i >= 0)
+                    classes.splice(i, 1);
+                else
+                    classes.push("opened");
+                    target.className = classes.join(" ");
+            }
+        }
+    });    
+}
+
+/* window.addEventListener('resize', function(event) {
+    
+}); */
